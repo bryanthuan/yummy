1f06cc6c8cd92d9e4511af7db4d3f264762d2d82
Brian Nguyen <thuann89@gmail.com>
2017-09-06 12:03:51 +0700

    Add flash message after save a store

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -15,7 +15,7 @@ exports.addStore = (req, res) => {
 exports.createStore = async (req, res) => {
   // res.json(req.body);
   const body = _.pick(req.body, ['name', 'description','tags']);
-  const store = new Store(body);
-  await store.save();
-  console.log('Saved.');
+  const store = await (new Store(body)).save();   
+  req.flash('success',`Successfully created ${store.name}. Care to leave a review ?`);
+  res.redirect(`/store/${store.slug}`);
 };
