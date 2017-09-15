dbed32784ab9e9f9c4217152b57256dc9abdd9b3
Brian Nguyen <thuann89@gmail.com>
2017-09-15 07:54:37 +0700

    Add more eslint rule defination

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,13 @@
 module.exports = {
     "extends": "airbnb-base",
+    "env": {
+        "browser": true,
+        "node": true
+    },
     "rules": {
+        "no-unused-vars":[1, { "argsIgnorePattern": "res|next|^err" }],
         "func-names": 0,
-        "no-underscore-dangle": 0
+        "no-underscore-dangle": 0,
+        "no-console": 0
     }
 };
